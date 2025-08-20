7246f825b96a64cff35d4d539e5ea63f8ac8b2ac
cheolki-yoon <dbscjfrl122422@gmail.com>
2025-08-20 15:10:09 +0900

    08/20 - 지그등록 버튼 색변경

--- lib/screens/warehouse_jigs_page.dart
+++ lib/screens/warehouse_jigs_page.dart
@@ -525,6 +525,7 @@ class _WarehouseJigsPageState extends State<WarehouseJigsPage> {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => _showAddOrEditJigDialog(),
         label: const Text('+ 지그 등록'),
+        backgroundColor: Colors.white,
       ),
     );
   }
